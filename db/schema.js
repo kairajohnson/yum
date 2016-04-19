bb382e4d98465a9cd4279791c2e26aed0db33fa3
KAIRA N. JOHNSON <KJOHNSON1913@GMAIL.COM>
2016-04-18 22:29:01 -0400

    created schema and models

--- db/schema.js
+++ db/schema.js
@@ -1,4 +1,40 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/yum');
+mongoose.connect = ('mongodb://localhost/restaurants');
+
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+
+db.on('error', function(err){
+  console.log(err);
+});
+
+db.once('open', function(){
+  console.log("database has been connected!")
+});
+
+process.exit();
+
+var Schema = mongoose.Schema,
+ObjectId = Schema.ObjectId
+
+var RestaurantSchema = new Schema({
+  name: String,
+    address: {
+      street: String,
+      zipcode: Number
+    },
+  yelp_url: String,
+  items:[ItemSchema]
+});
+
+var ItemSchema = new Schema({
+  title: String
+});
+
+var RestaurantModel = mongoose.model("Restaurant", RestaurantSchema)
+
+var ItemModel = mongoose.model("Item", ItemSchema)
+
+module.exports={
+  RestaurantModel: RestaurantModel,
+  ItemModel: ItemModel
+};
